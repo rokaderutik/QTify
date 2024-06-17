83b71bbb1d8c9ff53f9fa98dde51d49ba308c08e
rutikrokade <rutikrokade5084@gmail.com>
2024-06-17 15:50:33 +0530

    test commit

---
+++ qtify/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <hr />
+      <h1>Rutik Rokade Industries Ltd.</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
